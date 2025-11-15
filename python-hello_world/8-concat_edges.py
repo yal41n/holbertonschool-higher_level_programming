473cf73e65bf143a9590fd5fe801b18293a087cd
yal41n <dep3braw@gmail.com>
2025-11-15 19:29:34 +0000

    autocommit

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
+# do not create new variables or string literals
 print(str[39:66] + str[106:111] + str[:6])
