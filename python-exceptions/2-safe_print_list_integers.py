241fef04b642b4d8a147241f55f445a1666523f2
yal41n <dep3braw@gmail.com>
2025-11-17 09:54:08 +0000

    AUTO COMMIT

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,9 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            value = my_list[i]  # May raise IndexError
-            print("{:d}".format(value), end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
     print()
     return count
