479353f7168babc5d8e6577f2a455fcd991f2c13
yal41n <dep3braw@gmail.com>
2025-11-17 10:10:52 +0000

    AUTO COMMIT

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,7 +1,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
+    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))
