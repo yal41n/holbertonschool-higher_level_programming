36da63b4e74e1c94cb0cc6cd880e03300bb6503a
yal41n <dep3braw@gmail.com>
2025-11-21 06:32:29 +0000

    AUTO COMMIT

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,8 +1,17 @@
-def safe_print_division(a, b):
-    try:
-        result = a / b
-    except Exception:
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
-    return result
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """ divides element by element 2 lists """
+    new_list = []
+    for i in range(list_length):
+        tmp = 0
+        try:
+            tmp = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(tmp)
+    return (new_list)
