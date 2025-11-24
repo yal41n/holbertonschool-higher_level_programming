a9fa454a04e85293886ec0ff645137228caffa5e
yal41n <dep3braw@gmail.com>
2025-11-24 08:46:47 +0000

    AUTO COMMIT

--- python-inheritance/4-inherits_from.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module defining a function to check if an object is a subclass instance."""
-
-
-def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a class that inherits from a_class."""
-    return (issubclass(type(obj), a_class)
-            and type(obj) is not a_class)
