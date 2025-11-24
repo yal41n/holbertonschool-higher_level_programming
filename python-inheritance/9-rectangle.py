3ece9e7ba3e1866816a9d906d61d168888bb324f
yal41n <dep3braw@gmail.com>
2025-11-24 09:10:51 +0000

    AUTO COMMIT

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inheriting from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize Rectangle with validated width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return string representation of the Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
