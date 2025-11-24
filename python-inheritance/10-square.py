19ebc3ff8362734606f69275c8cd506170a350ea
yal41n <dep3braw@gmail.com>
2025-11-24 09:11:38 +0000

    AUTO COMMIT

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize Square with validated size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return area of the square."""
+        return self.__size * self.__size
