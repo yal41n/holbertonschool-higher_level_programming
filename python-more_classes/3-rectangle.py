ce57cd26f198fba7b119d8fe44c88601868c0a59
yal41n <dep3braw@gmail.com>
2025-11-24 06:42:25 +0000

    AUTO COMMIT

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -56,10 +56,9 @@ class Rectangle:
         return "\n".join("#" * self.__width for _ in range(self.__height))
 
     def __repr__(self):
-        """Return official representation."""
-        return "{}.{}({!r}, {!r})".format(
+        """Return default-like representation."""
+        return "<{}.{:s} object at {:s}>".format(
             self.__class__.__module__,
             self.__class__.__name__,
-            self.__width,
-            self.__height
+            hex(id(self))
         )
