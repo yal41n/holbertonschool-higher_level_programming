b75ffc7c423e82e87635c26107a9a23ba29fbefa
yal41n <dep3braw@gmail.com>
2025-11-24 08:45:35 +0000

    AUTO COMMIT

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,5 @@
 
 def inherits_from(obj, a_class):
     """Return True if obj is an instance of a class that inherits from a_class."""
-    return (
-        issubclass(type(obj), a_class) and type(obj) is not a_class
-    )
+    return (issubclass(type(obj), a_class)
+            and type(obj) is not a_class)
