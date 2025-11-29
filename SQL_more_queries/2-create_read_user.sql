2d525a9dbdb3593386b7649871a6c06ea81a5ca2
yal41n <dep3braw@gmail.com>
2025-11-29 14:07:01 +0000

    AUTO COMMIT

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,11 @@
+-- Create the database hbtn_0d_2 and the read-only user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user user_0d_2 with password user_0d_2_pwd
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege on hbtn_0d_2 to user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply the privilege changes
 FLUSH PRIVILEGES;
