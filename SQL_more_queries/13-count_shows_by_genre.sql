80585cb6aad959345ace2a6f3f60ffa2e6eff154
yal41n <dep3braw@gmail.com>
2025-11-29 15:51:21 +0000

    AUTO COMMIT

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
--- List all genres and the number of shows linked, excluding genres with no shows
-SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING number_of_shows > 0
+-- 13-count_shows_by_genre.sql
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
