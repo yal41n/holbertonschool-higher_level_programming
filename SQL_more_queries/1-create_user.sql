5f99830410c454ce1aff714adeb953e576954cd4
yal41n <dep3braw@gmail.com>
2025-11-29 14:01:49 +0000

    AUTO COMMIT

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- 1-create_user.sql
+-- Create user_0d_1 with all privileges if it does not exist
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
