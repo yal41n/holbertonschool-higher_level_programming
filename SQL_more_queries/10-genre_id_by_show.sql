c004a3fee1cd5d5597df4063345e28c5ede44f88
yal41n <dep3braw@gmail.com>
2025-11-29 14:22:46 +0000

    AUTO COMMIT

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT TV_SHOWS.TITLE, TV_SHOW_GENRES.GENRE_ID
+FROM TV_SHOWS
+INNER JOIN TV_SHOW_GENRES ON TV_SHOWS.ID = TV_SHOW_GENRES.SHOW_ID
+ORDER BY TV_SHOWS.TITLE ASC, TV_SHOW_GENRES.GENRE_ID ASC;
