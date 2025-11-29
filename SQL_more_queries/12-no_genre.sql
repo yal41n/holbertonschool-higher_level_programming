391f072e329082628213f6769872c110c844dbad
yal41n <dep3braw@gmail.com>
2025-11-29 15:49:30 +0000

    AUTO COMMIT

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
--- List shows without any genre linked
+-- 12-no_genre.sql
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
