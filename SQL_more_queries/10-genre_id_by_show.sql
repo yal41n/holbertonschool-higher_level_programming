9b2e46555232993c79fd0c40c360129be77e11f0
yal41n <dep3braw@gmail.com>
2025-11-29 14:23:32 +0000

    AUTO COMMIT

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,6 @@
+-- Lists all shows that have at least one genre linked
+-- Output: tv_shows.title - tv_show_genres.genre_id
+-- Sorted by tv_shows.title and tv_show_genres.genre_id in ascending order
 SELECT TV_SHOWS.TITLE, TV_SHOW_GENRES.GENRE_ID
 FROM TV_SHOWS
 INNER JOIN TV_SHOW_GENRES ON TV_SHOWS.ID = TV_SHOW_GENRES.SHOW_ID
