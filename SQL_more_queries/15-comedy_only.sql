51fdd070b85340b553cb5b3ee629458c4c312061
yal41n <dep3braw@gmail.com>
2025-11-29 15:55:37 +0000

    AUTO COMMIT

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,9 @@
--- List all Comedy shows
+-- 15-comedy_only.sql
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN genres ON genres.id = tv_show_genres.genre_id
-WHERE genres.name = 'Comedy'
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
