fc93d99e20850acf3ca36405d961fbed29af6bc6
yal41n <dep3braw@gmail.com>
2025-11-29 11:58:42 +0000

    AUTO COMMIT

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that adds all command-line arguments to a Python list and saves
+them to a JSON file (add_item.json).
+"""
+
+import sys
+from os.path import exists
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+if exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
